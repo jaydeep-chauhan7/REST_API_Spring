72cca36ab2c0b9e46437741270996d735525f0d6
JJ <speedtwisterj@gmail.com>
2022-04-19 03:16:47 +0530

    Securing REST API- using Spring Security in Spring Boot Application

---
+++ Demo01_SpringSecurity/src/main/java/com/infy/Demo01SpringSecurityApplication.java
@@ -0,0 +1,11 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01SpringSecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01SpringSecurityApplication.class, args);
+	}
+}
